d20a1aa8d4dda3269b178c0ae8a0e5dfc93a74e7
Lady-Marianne <mariana.andrea.lois@gmail.com>
2024-09-17 20:03:11 -0300

    Agregar setters y getters.

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,24 @@
+import com.aluracursos.screenmatch.modelos.Pelicula;
+
 public class Principal {
     public static void main(String[] args) {
-        Pelicula miPelicula = new Pelicula();
+        Pelicula Pelicula1 = new Pelicula();
+
+        Pelicula1.setNombre("Terminator");
+        Pelicula1.setFechaDeLanzamiento(1984);
+        Pelicula1.setDuracionEnMinutos(108);
+        Pelicula1.setIncluidoEnElPlan(true);
+
+        Pelicula1.evalua(7.8);
+        Pelicula1.evalua(10);
+        Pelicula1.evalua(9.8);
+
+        Pelicula1.muestraFichaTecnica();
+
+        System.out.println("Total de evaluaciones: " + Pelicula1.getTotalDeLasEvaluaciones());
+        System.out.println("Calificación: " + Pelicula1.calculaMedia());
+
+        String nombrePelicula = Pelicula1.getNombre();
+        System.out.println("Nombre: " + nombrePelicula);
     }
 }
